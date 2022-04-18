3fce008c6511f2c518b32df4cb0546388077d666
francoleone23 <fleone@udesa.edu.ar>
2022-04-18 15:48:51 -0300

    formato del buscador

---
+++ proyectotmfl/src/Components/Search/buscador.js
@@ -0,0 +1,36 @@
+import React, {Component} from "react"
+import "../Search/buscador.css"
+
+class Formulario extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			value: ""
+		}
+	}
+
+	notDefault(event){
+		console.log("Evito default");
+		event.preventDefault();
+	}
+
+	getDatos(datos){
+		this.setState({
+			value: datos.target.value
+		},
+		()=> this.props.filter(this.state.value)
+		)
+	}
+
+	render(){
+		return(
+		<form onSubmit= {(event)=> this.notDefault(event)}>
+			<input type="text" name="search" id="" placeholder="Que peli queres?" onChange = {(dato) => this.getDatos(dato)} value={this.state.value}/>
+			<button type="submit"><i className="fas fa-search"></i></button>
+		</form>     
+		);
+	}
+
+}
+
+export default Formulario;
